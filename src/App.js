b89217f2582c7c97461009309abdc219ccbfe006
iseungsoo07 <iseungsoo07@gmail.com>
2023-02-21 17:31:28 +0900

    [23.02.21] 공통 부분 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { Home } from "pages/home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "styles/common.css";
+import { Header } from "components/header";
+import { Navbar } from "components/navbar";
+import { Body } from "components/body";
+import { Footer } from "components/footer";
 
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-            </Routes>
+            <Header />
+            <Navbar />
+            <Body>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                </Routes>
+            </Body>
+            <Footer />
         </Router>
     );
 }
