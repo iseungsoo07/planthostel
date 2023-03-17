b5cf3749dba60c474876827365998d5f416b322d
seriokim <atomyoun@gmail.com>
2023-03-17 19:51:34 +0900

    [23.03.17] tel 회원정보 데이터 받아오기

--- src/components/Reserve.jsx
+++ src/components/Reserve.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
 import styles from "../styles/reserve.module.css";
 export default function Reserve() {
-  return <div className={styles.container}>회원정보</div>;
+  const {
+    isLoading,
+    error,
+    data: memberData,
+  } = useQuery(["memberData"], async () => {
+    return fetch(`data/memberData.json`).then((res) => res.json());
+  });
+
+  if (isLoading) return <p>loading...</p>;
+  if (error) return <p>{error}</p>;
+  console.log(memberData);
+  return (
+    <div className={styles.container}>
+      <h2>회원정보</h2>
+      <article className={styles.infoBox}>
+        <p>이름:{memberData[0].name}</p>
+        <p>이메일: {memberData[0].email}</p>
+        <p>휴대폰번호: {memberData[0].phone}</p>
+        <p>반려식물 등록수: {memberData[0].plant}</p>
+      </article>
+    </div>
+  );
 }
