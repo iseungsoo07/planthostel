c500cabd84ce2505c9fdc6df2806d29ebe8d308d
박세연 <wsx4379@naver.com>
2023-03-24 21:29:37 +0900

    [23.03.24] 예약자 성함, 번호 컴포넌트 생성

---
+++ src/components/ReserveInfo.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import styles from "styles/ReserveInfo.module.css";
+
+export default function ReserveInfo() {
+    const [form, setForm] = useState({ name: "", phone: "" });
+
+    const handleChange = (e) => {
+        if (e.target.name === "name") {
+            setForm((prev) => ({ ...prev, name: e.target.value }));
+        } else if (e.target.name === "phone") {
+            setForm((prev) => ({ ...prev, phone: e.target.value }));
+        }
+    };
+    return (
+        <form className={styles.container}>
+            <div className={styles.box}>
+                <p>예약자 성함</p>
+                <input
+                    type="text"
+                    name="name"
+                    value={form.name}
+                    placeholder="성함을 입력해주세요"
+                    onChange={handleChange}
+                />
+            </div>
+            <div className={styles.box}>
+                <p>예약자 휴대폰 번호</p>
+                <input
+                    type="text"
+                    name="phone"
+                    value={form.phone}
+                    placeholder="번호를 입력해주세요"
+                    onChange={handleChange}
+                />
+            </div>
+        </form>
+    );
+}
